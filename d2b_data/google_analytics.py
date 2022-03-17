2e35576906bc54ac6420c9d00ced39f76e874828
Analytics d2b <analytics@d2b.cl>
2022-03-17 19:23:35 +0000

    Update d2b_data/google_analytics.py

--- d2b_data/google_analytics.py
+++ d2b_data/google_analytics.py
@@ -1,3 +1,6 @@
-
-def __main__()
-    from pykemen.google.analytics_manager import Analytics
+def __main__():
+    __init__():
+        from pykemen.google.analytics_manager import Analytics
+        return Analytics
+        
+    
