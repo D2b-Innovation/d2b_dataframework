e39b8c978664bbd9fc09aa1901024aa080055a20
analytics67 <analytics@d2b.cl>
2025-11-26 12:07:04 -0300

    Agregamos método "_get_report" a la clase BigQuery y agregamos tqdm para mejorar visaulización.

--- d2b_data/Google_Bigquery.py
+++ d2b_data/Google_Bigquery.py
@@ -1,6 +1,6 @@
 import pandas
 from google.oauth2 import service_account
-
+from tqdm import tqdm
 
 class Google_Bigquery():
   def __init__(self,credentials_info=None,verbose=False):
@@ -25,6 +25,34 @@ class Google_Bigquery():
       '''
       if self.verbose:
         print(message)
+
+  def _get_data(self, query, project_id):
+      """
+      Función para descargar datos desde BigQuery
+      ARGS: 
+      query: <str> query,
+      project_id:  <str> project_id
+      """
+      self.debug('Downloading data from BigQuery...')
+      bar_type = 'tqdm' if self.verbose else None
+
+      try:
+        dataframe = pandas.read_gbq(
+            query,
+            project_id=project_id,
+            credentials=self.credentials,
+            dialect='standard',
+            progress_bar_type=bar_type
+        )
+        return dataframe
+      
+      except Exception as e:
+        print(f'Error downloading data from BigQuery: {e}')
+        return None
+
+      except Exception as e:
+        print(f'Error downloading data from BigQuery: {e}')
+        return None
 
   def dataframe_clean_cols(self,dataframe):
       columns = dataframe.columns
@@ -56,10 +84,23 @@ class Google_Bigquery():
           print("cleaning")
           dataframe = self.dataframe_clean_cols(dataframe)
           date_column = self.clean_date(date_column)
-      dataframe[date_column].astype(str)
-      for date in dataframe[date_column].unique():
-          self.debug('uploading {date_column}')
+      dataframe[date_column] = dataframe[date_column].astype(str)
+
+      unique_dates = dataframe[date_column].unique()
+
+      if self.verbose:
+          date_iterator = tqdm(unique_dates, desc='Uploading data by date') 
+      else:
+          date_iterator = unique_dates
+          
+      for date in date_iterator:
+          self.debug('uploading {date} data to BigQuery...'.format(date=date))
           iter_df = dataframe[dataframe[date_column]==date]
           text_date = date.replace("-","")
-          iter_df.to_gbq(destination+text_date,project_id=project_id,credentials=self.credentials,if_exists=if_exists)
+          iter_df.to_gbq(destination+text_date,
+                         project_id=project_id,
+                         credentials=self.credentials,
+                         if_exists=if_exists,
+                         progress_bar=False
+      )
       return
--- d2b_data/forecaster.py
+++ d2b_data/forecaster.py
@@ -12,7 +12,24 @@ class ProphetForecaster:
         DataFrame con las predicciones para las métricas especificadas.
     """
     def __init__(self, metricas):
-        self.metricas = metricas
+        self.metricas = metricas  # Aplicar getter y setter
+        self._validar_metricas()
+        self.resultados = pd.DataFrame()
+        self.prediccion = pd.DataFrame()
+        self.m = None
+    
+    def _validar_df(self, df):
+        """Valida que el DataFrame tenga las columnas necesarias.
+        
+        ARGS: 
+            df (DataFrame): DataFrame a validar.
+        RAISES:
+            ValueError: Si faltan columnas necesarias.
+        """
+        columnas_necesarias = ['date'] + self.metricas
+        for col in columnas_necesarias:
+            if col not in df.columns:
+                raise ValueError(f"El DataFrame debe contener la columna '{col}'")
 
     def generar_pronostico(self, df, dias):
         """Genera pronósticos para las métricas especificadas usando Prophet.
@@ -23,6 +40,7 @@ class ProphetForecaster:
         RETURNS:
             DataFrame con las predicciones para las métricas especificadas.
         """
+        # Agregar validador de columnas
         print(f"Estoy prediciendo las columnas: {self.metricas}")
         resultados = pd.DataFrame()
     
@@ -59,4 +77,18 @@ class ProphetForecaster:
             print(f" Forecast para {metric} por {dias} listo")
 
         return resultados.sort_values('date').round()
+    
+
+    # Modificaciones: Agregar Yhat lower YHat upper.
+    # Una opción independiente en cada producto del forecast.
+    # fig1 = m.plot(forecast) <- clase que retorna el plot. 
+    # fig2 = m.plot_components(forecast)
+    # Pasar DataFrame y no métricas en el init.
+
+
+    
+
+
+
+
         
