7bdb990285d3f1b1c97a8f05341685fdc5f44e8d
analytics67 <analytics@d2b.cl>
2025-12-24 10:54:04 -0300

    Añadimos método para revisar datos en clase Prophet

--- d2b_data/ProphetForecaster.py
+++ d2b_data/ProphetForecaster.py
@@ -4,32 +4,52 @@ from prophet import Prophet
 
 # Aquí agregamos los imports que vamos a usar
 class ProphetForecaster:
-    """Clase para generar pronósticos usando Prophet.
-    
-    ARGS: 
-        metricas (list): Lista de métricas a predecir.
-    RETURNS:
-        DataFrame con las predicciones para las métricas especificadas.
-    """
-    def __init__(self, metricas):
-        self.metricas = metricas  # Aplicar getter y setter
-        self._validar_metricas()
-        self.resultados = pd.DataFrame()
-        self.prediccion = pd.DataFrame()
-        self.m = None
-    
-    def _validar_df(self, df):
-        """Valida que el DataFrame tenga las columnas necesarias.
-        
-        ARGS: 
-            df (DataFrame): DataFrame a validar.
-        RAISES:
-            ValueError: Si faltan columnas necesarias.
+    """Class for generating forecasts using Prophet.
+
+        Args:
+            metrics (list): List of metrics to be predicted.
+
+        Returns:
+            DataFrame: A DataFrame containing the predictions for the specified metrics.
         """
-        columnas_necesarias = ['date'] + self.metricas
-        for col in columnas_necesarias:
-            if col not in df.columns:
-                raise ValueError(f"El DataFrame debe contener la columna '{col}'")
+    def __init__(self, metricas):
+        self.df_beforepedict = df_to_predict
+        self.df_postpredict  = None
+        self.m = None
+        self.display = None
+        self.df_ready = None
+
+    
+    def _data_validation(self, df_topredict):
+        """Validates the DataFrame data.
+
+        Args:
+            df_to_predict (DataFrame): The DataFrame to be validated.
+        """
+        date_options = ['date', 'fecha']
+
+        metrics_in_df = [col for col in df_topredict.columns if col not in date_options]
+        date_in_df = [col for col in df_topredict.columns if col in date_options]
+        prophet_dataframe = df_topredict.copy()
+
+        if not date_in_df:
+            raise ValueError("No date columns found in the DataFrame. Please upload a 'date' or 'fecha' column.")
+
+        date_to_predict = date_in_df[0]
+        prophet_dataframe[date_to_predict] = pd.to_datetime(prophet_dataframe[date_to_predict], format='%Y-%m-%d')
+        prophet_dataframe.rename(columns={date_to_predict: 'ds'}, inplace=True)
+        print( f" Using {date_to_predict} as date column")
+        
+        for col in metrics_in_df:
+            prophet_dataframe[col] = pd.to_numeric(prophet_dataframe[col], errors='coerce')
+            if prophet_dataframe[col].isnull().any():
+                print(f" Column {col} has Null values, check for data integrity")
+
+        new_date = ['ds']
+        df_final_cols = new_date + metrics_in_df
+        prophet_dataframe = prophet_dataframe[df_final_cols]
+        
+        self.df_ready = prophet_dataframe
 
     def generar_pronostico(self, df, dias):
         """Genera pronósticos para las métricas especificadas usando Prophet.
