b03a93b5442ee4be35b07ea9fce7c7aad902186f
NachoRobDev <analytics@d2b.cl>
2026-03-25 16:38:34 -0300

    Quitamos los verbose pedorros

--- d2b_data/Google_Spreadsheet.py
+++ d2b_data/Google_Spreadsheet.py
@@ -10,21 +10,10 @@ class Google_Spreadsheet:
     self.url_id  = url_id
     
     if use_service_account:
-        # Modo ROBOT (Service Account)
-        # El manager usa 'client_secret' como la ruta al key.json
         arg_secrets = credentials_path
-        arg_token = None # En tu lógica nueva de SA, esto se permite ser None porque entra al if use_sa primero
+        arg_token = None 
     else:
-        # Modo HUMANO (Legacy)
-        # TRUCO: Para evitar que Token_MNG salte al modo Cloud Run (ADC),
-        # debemos pasarle algo en ambos argumentos.
-        
-        # 1. 'token': Es tu archivo credentials_path real (el json con el token).
         arg_token = credentials_path
-        
-        # 2. 'client_secret': Le pasamos la misma ruta para que no sea None.
-        # ¿Por qué funciona? Porque Token_MNG revisa si 'arg_token' existe como archivo.
-        # Si existe (y existe), IGNORA el client_secret. Así que pasamos el mismo path para cumplir el protocolo.
         arg_secrets = credentials_path
     self.token_manager = Google_Token_MNG(
         client_secret=arg_secrets, 
@@ -42,13 +31,12 @@ class Google_Spreadsheet:
     return request
 
   def read_data_dataframe(self,spreadsheetId,range_name):
-    # Agregué un try/except básico porque esto falla si el rango está vacío
     try:
         request = self.service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=range_name)
         response = request.execute()
         
         if 'values' not in response:
-            return pd.DataFrame() # Retorna vacío si no hay datos
+            return pd.DataFrame() 
             
         df_response = pd.DataFrame(response.get('values'))
         df_response.columns = df_response.iloc[0]
@@ -103,7 +91,6 @@ class Google_Spreadsheet:
     return True
 
   def update_data(self, spreadsheet_id, range_index, data_list):
-    # Corrección menor: valueInputOption es obligatorio
     body_request = {'values': data_list}
     self.service.spreadsheets().values().update(
         spreadsheetId=spreadsheet_id, 
