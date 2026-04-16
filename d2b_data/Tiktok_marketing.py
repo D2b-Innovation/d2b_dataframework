2c52ff849c573638b9725887c0aac95b949912c7
NachoRobDev <analytics@d2b.cl>
2026-04-16 12:34:45 -0400

    Actualizamos el método get_authorized_advertisers para que sean opcionales el secret y el app_id

--- d2b_data/Tiktok_marketing.py
+++ d2b_data/Tiktok_marketing.py
@@ -23,9 +23,10 @@ class TikTokMarketing():
         }
 
         self.verbose.log("Tiktok Class instanciated with token")
-    
+
     def get_access_token(self, app_id: str, secret: str, auth_code: str):
         """Intercambia el auth_code por un access_token"""
+
         url = f"{self.endpoint_base}oauth2/access_token/"
         payload = {
             "app_id": app_id,
@@ -34,8 +35,10 @@ class TikTokMarketing():
         }
         response = requests.post(url, json=payload)
         data = response.json()
-        
+
         if data.get("code") == 0:
+            self.app_id = app_id
+            self.secret = secret
             self.token = data['data']['access_token']
             self.headers["Access-Token"] = self.token
             self.verbose.log(" Token obtenido y actualizado en la clase.")
@@ -43,20 +46,24 @@ class TikTokMarketing():
         else:
             self.verbose.log(f" Error obteniendo token: {data.get('message')}")
             return None
-    
-    def get_authorized_advertisers(self, app_id: str, secret: str):
+
+    def get_authorized_advertisers(self, app_id: str | None = None, secret: str | None = None):
         """Returns a list with the advertisers that the token has access to"""
         url=f"{self.endpoint_base}oauth2/advertiser/get/"
 
+        if app_id and secret:
+            self.app_id = app_id
+            self.secret = secret
+
+        if not self.app_id or not self.secret:
+            self.verbose.log(" You must provide add_id or secret to retrieve accounts")
+            return []    
+        
         params = {
-            "app_id": app_id,
-            "secret": secret
+            "app_id": self.app_id,
+            "secret": self.secret
         }
 
-        if not app_id or not secret:
-            self.verbose.log(" You must provide add_id or secret to retrieve accounts")
-            return []
-        
         response = requests.get(url, headers=self.headers, params=params)
         data = response.json()
         if data.get("code") == 0:
@@ -69,7 +76,7 @@ class TikTokMarketing():
         url = f"{self.endpoint_base}report/integrated/get/"
 
         self.verbose.log(f" Calling the TikTok API v1.3 for dates:{params.get('start_date')} -> {params.get('end_date')}")
-        
+
 
         for attempt in range(max_retries):
             try:
@@ -154,3 +161,40 @@ class TikTokMarketing():
           self.verbose.log("No data was extracted")
           return pd.DataFrame()
 
+    def get_report_json(self, params: dict, max_retries: int = 5):
+        """Low level calling API method"""
+
+        url = f"{self.endpoint_base}report/integrated/get/"
+
+        self.verbose.log(f" Calling the TikTok API v1.3 for dates:{params.get('start_date')} -> {params.get('end_date')}")
+
+
+        for attempt in range(max_retries):
+            try:
+              self.verbose.log(f" Calling TikTok API: {params.get('start_date')} -> {params.get('end_date')} (Intento {attempt+1})")
+              response = requests.get(url, headers=self.headers, params=params)
+
+              if response.status_code == 429:
+                  wait_time = (2 ** attempt) + random.random()
+                  self.verbose.log(f"Rate limit exceeded. Waiting for {wait_time} seconds before retrying.")
+                  time.sleep(wait_time)
+                  continue
+
+              response.raise_for_status()
+              data = response.json()
+
+              if data.get("code") != 0:
+                self.verbose.log(f"Error en API de TikTok: {data.get('message')} (Code: {data.get('code')})")
+                return None
+
+              return data
+
+            except requests.exceptions.HTTPError as e:
+                  self.verbose.log(f"HTTP Error: {e}")
+                  return None
+            except Exception as e:
+              self.verbose.log(f"Error calling the API: {e}")
+              return None
+
+# Token de autorización similar a el ga4 (casilla con input para que usuario llene y entregue la token)
+# Implementar un get_report_raw en estado json expuesto público (para debugeo)
